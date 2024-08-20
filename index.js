ef2f4a893cdc0b0dd38785c98c3d22cf28020955
ProShakil <developershakil27@gmail.com>
2024-08-20 23:21:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const express = require('express');
+const { scrapeLogic } = require('./scrapeLogic');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+app.get('/scrape', async (req, res) => {
+  try {
+    // Call scrapeLogic and wait for it to complete
+    await scrapeLogic();
+    res.send('Scraping completed.');
+  } catch (error) {
+    console.error('Error during scraping:', error);
+    res.status(500).send('An error occurred during scraping.');
+  }
 });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+app.get('/', (req, res) => {
+  res.send('Render Puppeteer server is up and running!');
 });
 
 app.listen(PORT, () => {
